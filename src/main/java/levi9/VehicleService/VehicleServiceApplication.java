253dc837711d0635305ac0ff62b251faf2cfdcc7
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-03-28 10:43:35 +0200

    feat: added discovery server and feign

--- src/main/java/levi9/VehicleService/VehicleServiceApplication.java
+++ src/main/java/levi9/VehicleService/VehicleServiceApplication.java
@@ -2,12 +2,22 @@ package levi9.VehicleService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class VehicleServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleServiceApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String example(){
+
+		return "aaaaaaaaaaaaaaaaaaaaaa";
+	}
 }
