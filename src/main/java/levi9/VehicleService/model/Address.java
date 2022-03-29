223b722f2b602d26a0d2559ee741f5319840b3f9
anagrahovac <grahovac.ana99@gmail.com>
2022-03-29 11:11:39 +0200

    improvement: constructors, getters and setters added with lombok annotations

--- src/main/java/levi9/VehicleService/model/Address.java
+++ src/main/java/levi9/VehicleService/model/Address.java
@@ -1,9 +1,16 @@
 package levi9.VehicleService.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Address {
     @Id
@@ -22,54 +29,6 @@ public class Address {
 
     private double latitude;
 
-    public Address() {
-    }
 
-    public String getStreet() {
-        return street;
-    }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber;
-    }
-
-    public String getTown() {
-        return town;
-    }
-
-    public void setTown(String town) {
-        this.town = town;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
 }
