f37e2d39b1058193a0de64fcef348ff22f0201d3
anagrahovac <grahovac.ana99@gmail.com>
2022-04-01 09:22:03 +0200

    improvement: send dto object as a response

--- src/main/java/levi9/VehicleService/service/impl/VehicleServiceImpl.java
+++ src/main/java/levi9/VehicleService/service/impl/VehicleServiceImpl.java
@@ -1,12 +1,13 @@
 package levi9.VehicleService.service.impl;
 
+import levi9.VehicleService.dto.VehicleDto;
 import levi9.VehicleService.model.Vehicle;
 import levi9.VehicleService.repository.VehicleRepository;
 import levi9.VehicleService.service.RentService;
 import levi9.VehicleService.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.modelmapper.ModelMapper;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,13 +17,14 @@ public class VehicleServiceImpl implements VehicleService {
     private VehicleRepository vehicleRepository;
     @Autowired
     private RentService rentService;
+    private final ModelMapper mapper = new ModelMapper();
 
     @Override
-    public List<Vehicle> findAvailableVehicles() {
-        List<Vehicle> availableVehicles = new ArrayList<>();
+    public List<VehicleDto> findAvailableVehicles() {
+        List<VehicleDto> availableVehicles = new ArrayList<>();
         for(Vehicle v : vehicleRepository.findAll()){
             if(!rentService.isVehicleRented(v.getId()))
-                availableVehicles.add(v);
+                availableVehicles.add(mapper.map(v, VehicleDto.class));
         }
         return  availableVehicles;
     }
