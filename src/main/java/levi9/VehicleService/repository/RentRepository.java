61ee322b8abb4a1d45ab99c2d1fdb8aedf19d28c
anagrahovac <grahovac.ana99@gmail.com>
2022-04-01 11:44:21 +0200

    improvement: find rent by vehicle id

--- src/main/java/levi9/VehicleService/repository/RentRepository.java
+++ src/main/java/levi9/VehicleService/repository/RentRepository.java
@@ -3,5 +3,9 @@ package levi9.VehicleService.repository;
 import levi9.VehicleService.model.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface RentRepository extends JpaRepository<Rent, Long> {
+    List<Rent> findByVehicleId(Long id);
 }
