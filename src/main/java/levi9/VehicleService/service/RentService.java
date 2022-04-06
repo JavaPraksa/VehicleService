a7c473b901781dbee0595541afe113634019ae94
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-04-06 11:07:25 +0200

    feat: rent history

--- src/main/java/levi9/VehicleService/service/RentService.java
+++ src/main/java/levi9/VehicleService/service/RentService.java
@@ -1,7 +1,10 @@
 package levi9.VehicleService.service;
 
 import levi9.VehicleService.dto.FinishRentDto;
+import levi9.VehicleService.dto.RentDto;
 import levi9.VehicleService.dto.RentedVehicleDto;
+
+import java.util.List;
 
 public interface RentService {
     boolean isVehicleRented(Long vehicleId);
@@ -9,4 +12,6 @@ public interface RentService {
     RentedVehicleDto getCurrentlyRentedVehicle(Long clientId);
 
     void finishRent(FinishRentDto finishRent);
+
+    List<RentDto> getRentHistory(Long clientId);
 }
