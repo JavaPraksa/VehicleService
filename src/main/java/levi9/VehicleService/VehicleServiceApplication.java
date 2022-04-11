a13a04fdcaa4a6821de5959d8ee7df44f01385f7
anagrahovac <grahovac.ana99@gmail.com>
2022-04-11 13:51:09 +0200

    feat: add notification service api to vehicle service

--- src/main/java/levi9/VehicleService/VehicleServiceApplication.java
+++ src/main/java/levi9/VehicleService/VehicleServiceApplication.java
@@ -1,15 +1,18 @@
 package levi9.VehicleService;
 import levi9.UserService.api.UserServiceApi;
+import levi9.NotificationService.api.NotificationServiceApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.Notification;
+
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients(clients = {UserServiceApi.class})
+@EnableFeignClients(clients = {UserServiceApi.class, NotificationServiceApi.class})
 @RestController
 public class VehicleServiceApplication {
 
